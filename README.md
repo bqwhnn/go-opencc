6bf4aeba010e49b72971bdd3a512ab9861bd3eef
caiyangmin <18819457397@163.com>
2019-06-19 10:48:40 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # go-opencc
 opencc wrapper for Golang
+
+> go run opencc.go server
+
+or
+> go run opencc.go [filepath1 filepath2 ...]
--- opencc.go
+++ opencc.go
@@ -1,37 +1,53 @@
 package main
 
 import (
-    "os"
-    "fmt"
-    "os/exec"
-    "path/filepath"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"syscall"
 	"time"
 )
 
 const (
 	config = "s2t"
-	in = "test"
 	suffix = ""
 )
 
 var (
-	dir string
+	dir   string
+	count int
+	paths = []string{
+		"F:/example",
+	}
 )
 
 func main() {
 	var err error
 	dir, err = os.Getwd()
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println(dir, err)
 	}
 
-	path := dir + "/" + in
-
-	err = filepath.Walk(path, convert)
-	if err != nil {
-		fmt.Printf("filepath.Walk error %v\n", err)
+	if len(os.Args) < 2 {
+		return
 	}
+
+	filepaths := []string{}
+	if os.Args[1] == "server" {
+		filepaths = paths
+	} else {
+		filepaths = os.Args[1:]
+	}
+
+	for _, path := range filepaths {
+		err = filepath.Walk(path, convert)
+		if err != nil {
+			fmt.Printf("filepath.Walk error %v\n", err)
+		}
+	}
+
+	fmt.Printf("total file count : %d\n", count)
 
 	time.Sleep(5 * time.Second)
 }
@@ -52,11 +68,13 @@ func convert(fp string, f os.FileInfo, err error) error {
 	fmt.Println(fp)
 
 	command := dir + "/opencc-1.0.4/bin/opencc.exe -i " + fp + " -o " + fp + " -c " + dir + "/opencc-1.0.4/share/opencc/" + config + ".json"
-	cmd := exec.Command("cmd.exe", "/c", "start " + command)
-	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow:true}
+	cmd := exec.Command("cmd.exe", "/c", "start "+command)
+	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
 	if err := cmd.Run(); err != nil {
 		return err
 	}
+
+	count = count + 1
 
 	return nil
 }
